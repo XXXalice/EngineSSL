ddbf88fa7562e177f32cc643cfb5c2793d3424f7
梅宮 <euthanasia045@gmail.com>
2018-10-11 10:03:36 +0900

    add exector

--- enginessl/exe.py
+++ enginessl/exe.py
@@ -2,3 +2,11 @@ import crawler as cr
 import os
 import sys
 
+clawler = cr.Clawler()
+if len(sys.argv) == 2:
+    clawler.crawl(keyword=sys.argv[1], num='')
+elif len(sys.argv == 3):
+    clawler.crawl(keyword=sys.argv[2], num=sys.argv[3])
+else:
+    print("error.")
+    exit()
