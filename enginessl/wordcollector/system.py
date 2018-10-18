1fa1f2cef643a29c826a702b95b76491ad3d6ffb
梅宮 <euthanasia045@gmail.com>
2018-10-18 14:03:56 +0900

    fix wordcollector

--- enginessl/wordcollector/system.py
+++ enginessl/wordcollector/system.py
@@ -4,13 +4,14 @@ class WordCollector(kernel.Kernel):
 
     def __init__(self):
         super().__init__('wikipedia.bin')
-        self.fnc = ['super().get_sim({},{})', 'super().get_notsim({},{})']
 
     def get_words(self, word, num, sim=False):
         if sim == False:
-            word_query = "'" + word + "'"
-            res = exec(self.fnc[1].format(word_query, num))
-            return res
+            res = super().get_notsim(word, num)
+        else:
+            res = super().get_sim(word, num)
+        return res
+
 
 
 
