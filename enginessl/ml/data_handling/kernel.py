180c6fd9fded98d7a163c305a28a4ea9fe773de1
梅宮 <euthanasia045@gmail.com>
2018-10-30 22:05:07 +0900

    complete base of data_handler

--- enginessl/ml/data_handling/kernel.py
+++ enginessl/ml/data_handling/kernel.py
@@ -3,6 +3,17 @@ import glob
 import sys
 import os
 from keras.preprocessing.image import load_img, img_to_array, array_to_img, save_img , ImageDataGenerator
+from keras.utils import to_categorical
+
+# Generate conflicting images fully automatically :)
+
+# [usage]
+# 2 step (Please set param.yaml in advance.)
+#
+# instance = OpponentImage(Kernel):
+# instance.return_datafile()
+#
+# too easy ! :)
 
 class Kernel():
 
@@ -32,16 +43,19 @@ class Kernel():
         self.y_train = []
         self.x_test = []
         self.y_test = []
-        data_container = [self.x_train, self.x_test]
-        label_container = [self.y_train, self.y_test]
         size = [self.params['ml']['img_size_xy']]*2 if not self.params['ml']['img_size_xy'] == None else size
-        for label, valid in enumerate((self.x_train_raw, self.x_test_raw)):
+        for label, valid in enumerate([self.x_train_raw, self.x_test_raw]):
             for img_path in valid:
                 try:
                     img = load_img(img_path, grayscale=gray, target_size=tuple(size))
                     img_bin = img_to_array(img)
-                    data_container[label].append(img_bin)
-                    label_container[label].append(0)
+                    # data_container[label].append(img_bin)
+                    if label == 0:
+                        self.x_train.append(img_bin)
+                        self.y_train.append([0])
+                    elif label == 1:
+                        self.x_test.append(img_bin)
+                        self.y_test.append([0])
                 except:
                     print('cant preprocessed image.[{}]'.format(img_path))
                     continue
@@ -49,9 +63,10 @@ class Kernel():
                     print('encoded img.[{}]'.format(img_path))
             # self.x_train = list(map(lambda img_bin: np.float16(img_bin)/255, self.x_train))
             # self.x_test = list(map(lambda img_bin: np.float16(img_bin) / 255, self.x_test))
-            self.x_train = list(map(lambda img_bin: precision(img_bin) / 255, self.x_train))
-            self.x_test = list(map(lambda img_bin: precision(img_bin) / 255, self.x_test))
+            self.x_train = list(map(lambda img_bin: np.ravel(precision(img_bin) / 255), self.x_train))
+            self.x_test = list(map(lambda img_bin: np.ravel(precision(img_bin) / 255), self.x_test))
         print('data shape {}'.format(self.x_train[0].shape))
+        print('train {}  test {}'.format(len(self.x_train), len(self.x_test)))
 
     def labeling(self, one_hot=True):
         pass
@@ -76,33 +91,57 @@ class OpponentImage(Kernel):
         self.data_split()
         self.data_preprocess_basic()
         self.ancestors = [self.x_train, self.x_test]
+        self.ancestors_label = [self.y_train, self.y_test]
         self.decay = self.params['oppoimg']['decay']
+        self.mode = self.params['oppoimg']['mode']
         # self.__gc_superclassvals()
-        self.datas = self.make_fuzzyimg(decay=self.decay, effect='s_random')
+        self.datas = self.make_fuzzyimg(decay=self.decay, effect=self.mode)
 
     def make_fuzzyimg(self, decay, effect):
         import effect_func as ef
+
         e_dict = {
             's_random': lambda x: ef.simple_random(x),
-            'swap': lambda x: ef.swap(x)
+            'swap': lambda x: ef.swap(x),
+            'as_random': lambda  x: ef.ancestral_scale_random(x),
+            'as_randomv2': lambda x: ef.ancestral_scale_random_v2(x)
         }
-        oppotrain = []
-        oppotest = []
-        for bins in self.ancestors:
-            for bin in bins:
-                effected_bin = e_dict[effect](bin)
-                print(effected_bin)
-                oppotrain.append(effected_bin)
-                break
-        # self.__test_show(oppotrain[0])
 
+        for i, img_bins in enumerate(self.ancestors):
+            flat_img_bins = [*map(lambda img_bin: np.ravel(img_bin), img_bins)]
+            for img_count, flat_img_bin in enumerate(flat_img_bins):
+                # print(flat_img_bin, flat_img_bin.shape)
+                # break
+                try:
+                    effected_bin = e_dict[effect](flat_img_bin)
+                    # self.ancestors[i].append(effected_bin)
+                    # self.ancestors_label[i].append([1])
+                    if i == 0:
+                        self.x_train.append(effected_bin)
+                        self.y_train.append([1])
+                    elif i == 1:
+                        self.x_test.append(effected_bin)
+                        self.y_test.append([1])
+                except:
+                    print('cant generate fuzzyimg.')
+                    continue
+                else:
+                    print('generated fuzzyimg.{}'.format(img_count+1))
+        print('fuzzy mode {}'.format(effect))
+        # self.__test_show(self.ancestors[0][150])
 
     def anal_ances(self):
         pass
 
+    def return_datafiles(self):
+        self.y_train = to_categorical(self.y_train, num_classes=2)
+        self.y_test = to_categorical(self.y_test, num_classes=2)
+        return (self.x_train, self.x_test, self.y_train, self.x_test)
+
+
     def __test_show(self, np_img):
         import matplotlib.pyplot as plt
-        ex_img = array_to_img(np_img)
+        ex_img = array_to_img(np_img.reshape(100,100,1))
         ex_img.save('test.png')
 
     def __gc_superclassvals(self):
@@ -121,4 +160,5 @@ if __name__ == '__main__':
     # k = Kernel()
     # k.data_split()
     # k.data_preprocess_basic()
-    oppi = OpponentImage()
+    # oppi = OpponentImage()
+    # datasets = oppi.return_datafiles()
