3483e91d2d798f1e58143ca4281e3316ce4e04e4
梅宮 <euthanasia045@gmail.com>
2019-03-02 19:14:36 +0900

    WIP network_easymode

--- enginessl/ml/system.py
+++ enginessl/ml/system.py
@@ -1,8 +1,24 @@
-import kernel as mlkernel
-import network
+from . import kernel as mlkernel
+from . import network
 
 class MachineLearning(mlkernel.Kernel):
 
     def __init__(self, param_path='param.yml'):
-        ml = mlkernel.Kernel(param_path)
+        self.Ml = mlkernel.Kernel(param_path)
 
+    def get_datas(self):
+        '''
+        :return: tuple(self.x_train, self.x_test, self.y_train, self.y_test)
+        '''
+        self.datas = self.Ml.get_origin_data()
+        return self.datas
+
+    def build_model(self):
+        self.model = self.Ml.generate_model()
+        return self.model
+
+    def train_model(self, model, datas):
+        self.Ml.training(model=model, datas=datas)
+
+    def fine_tuning_model(self):
+        pass
