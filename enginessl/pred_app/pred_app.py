25022d3650e34689a11c282252ab65f654724d75
梅宮 <euthanasia045@gmail.com>
2019-05-17 22:19:38 +0900

    WIP making predict app

--- enginessl/pred_app/pred_app.py
+++ enginessl/pred_app/pred_app.py
@@ -1,12 +1,16 @@
 import os
+from time import sleep
 from flask import Flask, request, send_from_directory, render_template, redirect
 from keras.models import Sequential, load_model
 from keras.preprocessing.image import img_to_array
 from werkzeug.utils import secure_filename
+from selenium.webdriver import Chrome
+import chromedriver_binary
+
 
 class PredApp:
     def __init__(self, *args):
-        self.app = Flask('predict')
+        self.app = Flask(__name__)
         self.classes = args if len(args) != 1 else args[0]
         self.upload_folder = './data/uploads/'
         os.makedirs(self.upload_folder, exist_ok=True)
@@ -15,7 +19,7 @@ class PredApp:
         self.port = 3553
         self.host = 'localhost'
 
-    def run(self, auto_startup=False):
+    def run(self):
         @self.app.route('/test')
         def admin_test():
             return render_template('admin.html')
@@ -30,32 +34,30 @@ class PredApp:
                     img_path = './data/uploads/' + fname
                     print(img_path)
                     return render_template('index.html', img_path=img_path)
+            else:
+                return render_template('index.html')
 
-        @self.app.route('/uploads/<filename>')
+        @self.app.route('/<filename>')
         def uploaded_file(filename):
             return send_from_directory(self.app.config['UPLOAD_FOLDER'], filename)
+        self.__startup_browser(port=self.port, host=self.host)
 
-        self.app.run(host='localhost', port=3553)
-        if auto_startup:
-            self.__startup_browser()
-
-
+    def __startup_browser(self, port=3553, host='localhost'):
+        try:
+            driver = Chrome()
+            url = 'http://{host}:{port}/'.format(host=host, port=port)
+            driver.get(url)
+        except:
+            self.app.run(host=host, port=port)
+        finally:
+            sleep(3)
+            driver.refresh()
 
     def __allowed_file(self, fname):
         return '.' in fname and fname.split('.', 1)[1] in self.allow_ext
 
-    def __startup_browser(self, port=3553, host='localhost'):
-        from selenium.webdriver import Chrome
-        import chromedriver_binary
-        driver = Chrome()
-        url = 'http://{host}:{port}'.format(host=host, port=port)
-        driver.get(url=url)
-
-
-
 
 if __name__ == '__main__':
-    print(os.getcwd())
     a = PredApp()
     a.app.debug=True
-    a.run(auto_startup=True)
+    a.run()
