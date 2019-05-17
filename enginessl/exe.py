cfc6b7ad7e851cbe4ba193fd34833ebefc86dc96
梅宮 <euthanasia045@gmail.com>
2019-05-17 21:37:11 +0900

    WIP making pred_apps

--- enginessl/exe.py
+++ enginessl/exe.py
@@ -1,13 +1,14 @@
 #!/usr/local/var/pyenv/versions/anaconda3-5.2.0/envs/ml/bin/python
 import sys
-import os
 from crawler import system as crawler_api
 from ml import system as ml_api
 from ml.data_handling import system as data_api
 from etc import system_metadata as opt
+from etc import wordart
 
 
 def main():
+    wordart.print_logo('big')
     if len(sys.argv) <= 1:
         print(opt.help)
         exit()
