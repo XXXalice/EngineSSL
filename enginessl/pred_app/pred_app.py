6423e2bbcda0b550fd0ccc93fbd6263cb22abdaf
梅宮 <euthanasia045@gmail.com>
2019-05-19 12:25:12 +0900

    accept pred_app imgs

--- enginessl/pred_app/pred_app.py
+++ enginessl/pred_app/pred_app.py
@@ -1,6 +1,6 @@
 import os
-from time import sleep
-from flask import Flask, request, send_from_directory, render_template
+import sys
+from flask import Flask, request, send_from_directory, render_template, redirect, url_for
 from keras.models import Sequential, load_model
 from keras.preprocessing.image import img_to_array
 from werkzeug.utils import secure_filename
@@ -12,7 +12,7 @@ class PredApp:
     def __init__(self, *args):
         self.app = Flask(__name__)
         self.classes = args if len(args) != 1 else args[0]
-        self.upload_folder = './data/uploads/'
+        self.upload_folder = './uploads/'
         os.makedirs(self.upload_folder, exist_ok=True)
         self.app.config['UPLOAD_FOLDER'] = self.upload_folder
         self.allow_ext = set(['jpeg', 'jpg', 'png', 'gif'])
@@ -20,38 +20,38 @@ class PredApp:
         self.host = 'localhost'
 
     def run(self):
-        @self.app.route('/test')
+        @self.app.route('/')
         def admin_test():
             return render_template('admin.html')
 
-        @self.app.route('/', methods=['GET', 'POST'])
+        @self.app.route('/pred', methods=['GET', 'POST'])
         def pred():
             if request.method == 'POST':
                 img_file = request.files['img_file']
                 if img_file and self.__allowed_file(img_file.filename):
                     fname = secure_filename(img_file.filename)
                     img_file.save(os.path.join(self.app.config['UPLOAD_FOLDER'], fname))
-                    img_path = './data/uploads/' + fname
+                    img_path = './uploads/' + fname
                     print(img_path)
                     return render_template('index.html', img_path=img_path)
+                else:
+                    return '''
+                    <p>許可されていない拡張子です</p>
+                    '''
             else:
                 return render_template('index.html')
 
-        @self.app.route('/<filename>')
-        def uploaded_file(filename):
-            return send_from_directory(self.app.config['UPLOAD_FOLDER'], filename)
+        @self.app.route('/uploads/<fname>')
+        def uploaded_file(fname):
+            return send_from_directory(self.app.config['UPLOAD_FOLDER'], fname)
         self.__startup_browser(port=self.port, host=self.host)
 
     def __startup_browser(self, port=3553, host='localhost'):
         try:
-            driver = Chrome()
-            url = 'http://{host}:{port}/'.format(host=host, port=port)
-            driver.get(url)
-        except:
             self.app.run(host=host, port=port)
-        finally:
-            sleep(3)
-            driver.refresh()
+        except Exception as e:
+            sys.stderr.write(str(e))
+            sys.exit(0)
 
     def __allowed_file(self, fname):
         return '.' in fname and fname.split('.', 1)[1] in self.allow_ext
