311cb586beb5179bff831f91964ec690fc1cfbb8
梅宮 <euthanasia045@gmail.com>
2019-07-22 19:12:51 +0900

    add noise_test

--- enginessl/test/noise_tester.py
+++ enginessl/test/noise_tester.py
@@ -1,8 +1,51 @@
 import inspect
+import argparse
 import os
 import sys
+import numpy as np
 REL_HIERARCHY = 2
-BASE_PATH = os.path.join(''.join(inspect.stack()[0][1].split('/')[:-REL_HIERARCHY]))
-EFFECT_MODULE = os.path.join(BASE_PATH, 'ml')
-sys.path.append(EFFECT_MODULE)
-from data_handling import effect_func
+toplevel_path = os.path.join('/'.join(os.path.abspath(__file__).split('/')[:-REL_HIERARCHY]))
+sys.path.append(os.path.join(toplevel_path, 'ml', 'data_handling'))
+import effect_func as ef
+from keras.preprocessing.image import load_img, img_to_array, array_to_img, save_img
+
+
+def make_noise(img_bin, mode):
+    e_dict = {
+        's_random': lambda x: ef.simple_random(x),
+        'n_random': lambda x: ef.normal_random(x),
+    }
+    # try:
+    effected = e_dict[mode](img_bin)
+    img_name = make_imgname(mode=mode)
+    # except Exception as e:
+    #     sys.stderr.write(str(e)+'\n')
+    #     exit(0)
+
+    return save_img(path=os.path.join('noise_test', img_name), x=effected)
+
+def make_imgname(mode):
+    tag = np.random.randint(0, 9999)
+    name = '{}_{:04}.png'.format(mode, tag)
+    return name
+
+def main():
+    img_path = './noise_test/test.png'
+    parser = argparse.ArgumentParser(description='noise_tester')
+    parser.add_argument('-i', '--img', help='img path.')
+    parser.add_argument('-m', '--mode', help='noise algorithm')
+    args = parser.parse_args()
+    if args.img != None:
+        img_path = args.img
+    img = load_img(img_path, color_mode='grayscale', target_size=(100, 100))
+    img_bin = img_to_array(img)
+    result = make_noise(img_bin=img_bin, mode=args.mode)
+    if result:
+        print('succsessed.')
+
+
+
+
+
+if __name__ == '__main__':
+    main()
