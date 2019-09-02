c2ab34068ba75fd17524fd5ccc1e25a4852d8cc7
梅宮 <euthanasia045@gmail.com>
2019-09-02 16:27:38 +0900

    add debug in exe.py

--- enginessl/exe.py
+++ enginessl/exe.py
@@ -54,6 +54,9 @@ def main():
     noise.make_noise()
     targets, not_targets = data.read_dirs(datas_dir=data.data_handling.datas_dir, target_label=target_label)
     x_train, x_test, y_train, y_test = data.get_builtup_data(targets=targets, not_targets=not_targets, flatten=False, color_mode='grayscale')
+    print(len(x_train), len(x_test), len(y_train), len(y_test))
+    print(y_train)
+    print(y_test)
     #ここまでok
     ml = ml_api.MachineLearning()
     model = ml.build_model()
