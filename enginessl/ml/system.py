f8a235ca70772626474e76a55bf8e3b3c39546b5
梅宮 <euthanasia045@gmail.com>
2019-10-09 15:57:00 +0900

    ml/system 不要な関数を削除

--- enginessl/ml/system.py
+++ enginessl/ml/system.py
@@ -9,20 +9,6 @@ class MachineLearning(mlkernel.Kernel):
         param_path = os.path.join('/'.join(inspect.stack()[0][1].split('/')[:-2]), 'param.yml')
         self.Ml = mlkernel.Kernel(param_path)
 
-    def get_datas(self):
-        '''
-        :return: tuple(x_train, x_test, y_train, y_test)
-        '''
-        self.datas = self.Ml.get_origin_data()
-        return self.datas
-
-    def get_preprocessed_data(self):
-        '''
-        :return: tuple(x_train, x_test, y_train, y_test) #分割済み
-        '''
-        self.datas = self.Ml.correct_datas()
-        return self.datas
-
     def build_model(self):
         self.model = self.Ml.generate_model()
         return self.model
