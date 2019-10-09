6eb86b8f746a6f9721e54be32519c523e0b49585
梅宮 <euthanasia045@gmail.com>
2019-10-09 16:00:56 +0900

    不要な変数を削除、不適切な変数名を変更

--- enginessl/ml/system.py
+++ enginessl/ml/system.py
@@ -7,15 +7,15 @@ class MachineLearning(mlkernel.Kernel):
 
     def __init__(self):
         param_path = os.path.join('/'.join(inspect.stack()[0][1].split('/')[:-2]), 'param.yml')
-        self.Ml = mlkernel.Kernel(param_path)
+        self.ml = mlkernel.Kernel(param_path)
 
     def build_model(self):
-        self.model = self.Ml.generate_model()
+        self.model = self.ml.generate_model()
         return self.model
 
     def train_model(self, model, datas, save_name, es=True):
         save_name = save_name.split('_')[0]
-        return self.Ml.training(model=model, datas=datas, save_name=save_name, es=es)
+        return self.ml.training(model=model, datas=datas, save_name=save_name, es=es)
 
     def fine_tuning_model(self):
         pass
