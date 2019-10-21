a0cd34b1d38838db23394a5212d408209f6dd87c
梅宮 <euthanasia045@gmail.com>
2019-10-21 16:08:23 +0900

    WIP app_system

--- enginessl/pred_app/app_system/kernel.py
+++ enginessl/pred_app/app_system/kernel.py
@@ -2,7 +2,19 @@ import inspect
 import os
 import yaml
 
-def bias_judgement(score):
+def preprocessing_judgement(pred):
+    """
+    前処理判断
+    :param pred: model.predict()の戻り値
+    :return: 未定
+    """
+    if pred[0].argmax() != 0:
+        # 判断がnot_targetだったら
+        params = read_yaml()
+        bias_judgement(score=pred[0].max(), threshold=params['predictapp']['bias'])
+
+
+def bias_judgement(score, threshold):
     pass
 
 
