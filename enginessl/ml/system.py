e23ce3e87b26d2cbc1f2341e029856c5b83001d8
梅宮 <euthanasia045@gmail.com>
2019-11-06 16:26:30 +0900

    グラフ保存機能の実行環境追加

--- enginessl/ml/system.py
+++ enginessl/ml/system.py
@@ -16,6 +16,9 @@ class MachineLearning(mlkernel.Kernel):
     def train(self, name, model, datas, es):
         return self.user_nn.train(name=name ,model=model, datas=datas, es=es)
 
+    def draw_graph(self, model_name):
+        self.draw_graph(model_name=model_name)
+
     def fine_tuning_model(self):
         pass
 
