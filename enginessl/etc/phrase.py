adadf345f4dad3d7e7e3d7491014b6e48b412b59
梅宮 <euthanasia045@gmail.com>
2019-11-12 16:57:34 +0900

    WIP テキスト解析 jsonの日本語対応

--- enginessl/etc/phrase.py
+++ enginessl/etc/phrase.py
@@ -1,7 +1,10 @@
 import json
 from lxml.etree import parse
 import os
-import urllib
+import urllib.request
+import urllib.parse
+import pprint
+import base64
 
 class UseApis:
     def __init__(self, base="", server_name=""):
@@ -10,7 +13,7 @@ class UseApis:
 
     def load_secrets(self, sec_path):
         try:
-            with open(sec_path, "rb") as sec:
+            with open(sec_path, mode="r", encoding="utf-8") as sec:
                 elems = [elem.rstrip(os.linesep) for elem in sec.readlines()]
             self.CLIENT_ID = elems[0]
             self.CLIENT_SEC = elems[1]
@@ -45,3 +48,29 @@ class UseApis:
         roots = tree.getroot()
         json_body = json.dumps(roots, indent=2)
         return json_body
+
+
+class YahooPhrase(UseApis):
+    def __init__(self):
+        base_url = "https://jlp.yahooapis.jp/KeyphraseService/V1/extract"
+        sec_path = "./YAHOO.sec"
+        super().__init__(base=base_url, server_name="keypharase")
+        self.load_secrets(sec_path=sec_path)
+
+    def execute(self, *q, flexible_mode=True):
+        if flexible_mode:
+            q = q[0]
+            queries = q.split(" ")
+            sentence = queries[0]
+        else:
+            sentence = q[0]
+        sentence = urllib.parse.quote(sentence)
+        self.construct_url(appid=self.CLIENT_ID, sentence=sentence, output="json")
+        resp = self.url_request()
+        return resp
+
+if __name__ == '__main__':
+    pharase = YahooPhrase()
+    sentence = input("いんぷーっと！：")
+    json = pharase.execute(sentence)
+    pprint.pprint(json)
